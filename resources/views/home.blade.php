8fb729f8b9e49ae9f5b767007147c89a91b7237d
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-12 17:33:33 +0200

    Stampa dei treni con ciclo foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
     <div class="row">
         @foreach ($trains as $train)
             <div class="col">
-                <h1>{{ $train->aziende }}</h1>
+                <h1>Azienda:{{ $train->aziende }}</h1>
+                <h1>Partenza:{{ $train->stazione_di_partenza }}</h1>
 
             </div>
         @endforeach
