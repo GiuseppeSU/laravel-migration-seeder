aed82d804522d732231ee250c7a23917080524fd
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-12 16:43:15 +0200

    Push di aggiornamento

--- database/migrations/2023_05_12_132135_create_trains_table.php
+++ database/migrations/2023_05_12_132135_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('aziende', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('partenza');
+            $table->dateTime('arrivo');
+            $table->unsignedMediumInteger('codice_treno');
+            $table->unsignedMediumInteger('numero_carrozze');
+            $table->boolean('ritardo');
+
+
             $table->timestamps();
         });
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
     <div class="row">
+        @foreach ($trains as $train)
+            <div class="col">
+                <h1>{{ $train->Aziende }}</h1>
+
+            </div>
+        @endforeach
 
     </div>
 @endsection
